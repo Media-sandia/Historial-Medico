8334e334d585309cf13fd52922000f10d286a803
Media-sandia <L161806.slp.tecnm.mx>
2021-06-29 12:47:04 -0500

    Actualizion del Layaut

--- resources/views/layouts/layaut.blade.php
+++ resources/views/layouts/layaut.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Inventario | Creo </title>
+    <title>Historial | Medico </title>
     <link rel="shortcut icon" type="image/png" href="favicon.ico">
     <!-- Select2 -->
     <link media="all" type="text/css" rel="stylesheet" href="assets/lib/css/select2.min.css">
